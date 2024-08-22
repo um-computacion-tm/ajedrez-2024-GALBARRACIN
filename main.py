a4122c02f152c7b5585dffb6afa9335b2c739c67
¨Gonzalo <albarracin.agn@gmail.com>
2024-08-22 20:14:04 -0300

    configuracion PyGame

--- main.py
+++ main.py
@@ -3,3 +3,24 @@ def suma (a, b):
 
 if __name__ == '__main__':
     print(suma(1, 2))
+
+#Ajedredez con modulo PyGame
+
+import pygame
+
+pygame.init()
+WIDTH = 1000
+HEIGHT = 900
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('¡Ajedrez para dos Jugadores!') #Titulo de la Ventana
+font = pygame.font.Font('freesansbold.ttf', 20) #Fuentes de textos, esta es de uso libre
+medium_font = pygame.font.Font('freesansbold.ttf', 40)
+big_font = pygame.font.Font('freesansbold.ttf', 50)
+timer = pygame.time.Clock() #Tiempo de Actualizacion del juego
+fps = 30 #Numero de fotogramas
+
+# bucle principal del juego
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('light yellow')
